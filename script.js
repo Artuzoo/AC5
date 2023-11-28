6d7f40416e48fbb612e471fb1c9c751ef0b4b0dc
Artuzoo <150751277+Artuzoo@users.noreply.github.com>
2023-11-28 20:38:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.botao button').addEventListener('click', function() {
+    this.style.display = 'none';
+
+    var siteContent = document.querySelector('#siteContent');
+            siteContent.style.visibility = 'visible';
+            siteContent.style.display = 'block';
+        });
